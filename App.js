8dfc45704e60aef56575733dbebebe2d387cc2a4
unknown <lengchhengpich6@gmail.com>
2023-03-29 15:13:37 +0700

    add Bottom Navigation

--- App.js
+++ App.js
@@ -1,28 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home';
-export default function App() {
-  state = {
-    myState: 'Hello'
-  }
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar } from "expo-status-bar";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./components/Home";
+function FirstScreen() {
+  return <Home />;
+}
+function SecondScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <Text>{this.state.myState}</Text>
-      <StatusBar style="auto" />
-      <Home />
+    <View>
+      <Text>This is SecondScreen</Text>
     </View>
   );
 }
+const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator();
+function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={FirstScreen}></Tab.Screen>
+          <Tab.Screen name="About" component={SecondScreen}></Tab.Screen>
+        </Tab.Navigator>
+        {/* <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={FirstScreen}></Drawer.Screen>
+        </Drawer.Navigator> */}
+      </NavigationContainer>
+    </>
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>Hello</Text>
+    //   <StatusBar backgroundColor = "yellow" />
+    //   <Home />
+    // </View>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    fontSize: 50
-  }
+    fontSize: 50,
+  },
 });
--- components/Home.js
+++ components/Home.js
@@ -5,26 +5,23 @@ import {
   TextInput,
   StyleSheet,
   View,
+  ScrollView,
 } from "react-native";
 export const Home = () => {
   state = {
     names: [
-      {
-        id: 0,
-        name: "Ben",
-      },
-      {
-        id: 1,
-        name: "Susan",
-      },
-      {
-        id: 2,
-        name: "Robert",
-      },
-      {
-        id: 3,
-        name: "Mary",
-      },
+      { name: "Ben", id: 1 },
+      { name: "Susan", id: 2 },
+      { name: "Robert", id: 3 },
+      { name: "Mary", id: 4 },
+      { name: "Daniel", id: 5 },
+      { name: "Laura", id: 6 },
+      { name: "John", id: 7 },
+      { name: "Debra", id: 8 },
+      { name: "Aron", id: 9 },
+      { name: "Ann", id: 10 },
+      { name: "Steve", id: 11 },
+      { name: "Olivia", id: 12 },
     ],
   };
   alertItemName = (item) => {
@@ -33,15 +30,15 @@ export const Home = () => {
   return (
     <>
       <View>
-        {this.state.names.map((item, index) => (
-          <TouchableOpacity
-            key={item.id}
-            style={styles.container}
-            onPress={() => this.alertItemName(item)}
-          >
-            <Text style={styles.text}>{item.name}</Text>
-          </TouchableOpacity>
-        ))}
+        <ScrollView>
+          {this.state.names.map((item, index) => (
+            <View>
+              <View key={item.id} style={styles.item}>
+                <Text onPress={() => alert("ji")}>{item.name}</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </>
   );
@@ -50,13 +47,14 @@ export const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    marginTop: 3,
-    backgroundColor: "#d9f9b1",
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-  },
-  text: {
-    color: "#4f603c",
+    padding: 30,
+    margin: 2,
+    borderColor: "#2a4944",
+    borderWidth: 1,
+    backgroundColor: "#d2f7f1",
   },
 });
